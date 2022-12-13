7231829a6e4b7a098339497187f52abe2b4f5a75
Biene-code <120476915+Biene-code@users.noreply.github.com>
2022-12-13 23:31:55 +0900

    Add files via upload

---
+++ TODOリスト/TODO.js
@@ -0,0 +1,40 @@
+const taskValue = document.getElementsByClassName('task_value')[0];
+const taskSubmit = document.getElementsByClassName('task_submit')[0];
+const taskList = document.getElementsByClassName('task_list')[0];
+
+// 追加ボタンを作成
+const addTasks = (task) => {
+  // 入力したタスクを追加・表示
+  const listItem = document.createElement('li');
+  const showItem = taskList.appendChild(listItem);
+  showItem.innerHTML = task;
+
+
+  // タスクに削除ボタンを付与
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = 'Del';
+  listItem.appendChild(deleteButton);
+
+
+  // 削除ボタンをクリックし、イベントを発動（タスクが削除）
+  deleteButton.addEventListener('click', evt => {
+    evt.preventDefault();
+    deleteTasks(deleteButton);
+  });
+};
+
+
+// 削除ボタンにタスクを消す機能
+const deleteTasks = (deleteButton) => {
+  const chosenTask = deleteButton.closest('li');
+  taskList.removeChild(chosenTask);
+};
+
+
+// 追加ボタンをクリックし、イベントを発動（タスクが追加）
+taskSubmit.addEventListener('click', evt => {
+  evt.preventDefault();
+  const task = taskValue.value;
+  addTasks(task);
+  taskValue.value = '';
+});
